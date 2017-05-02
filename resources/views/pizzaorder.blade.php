85cdc20280c7f506ef8956a6ac0f5bd1bdcf5e0d
monikasmilga <tuuli705@gmail.com>
2017-05-02 14:12:44 +0300

    Installed Forms & HTML and created a blade for orders

---
+++ resources/views/pizzaorder.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Picos užsakymas</title>
+</head>
+<body>
+
+{!! Form::open(['url' => 'foo/bar']) !!}
+//
+{!! Form::close() !!}
+
+
+
+</body>
+</html>
