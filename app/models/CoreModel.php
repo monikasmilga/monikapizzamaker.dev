751258f9f33ad17a2511f57e3fc9e4b4f269f3b6
monikasmilga <tuuli705@gmail.com>
2017-05-03 14:11:54 +0300

    Added boot function that generates UUID

--- app/models/CoreModel.php
+++ app/models/CoreModel.php
@@ -1,12 +1,23 @@
 <?php
+
 namespace App\models;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 
 class CoreModel extends Model
 {
-use SoftDeletes;
+    use SoftDeletes;
 
-public $incrementing = false;
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = Uuid::uuid4();
+        });
+    }
 }
