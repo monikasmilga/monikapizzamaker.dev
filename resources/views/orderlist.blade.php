606647034892fbbd893824b6d52fa5d5208d2ea1
monikasmilga <tuuli705@gmail.com>
2017-05-08 00:13:40 +0300

    Installed bootstrap and changed orderlist style

--- resources/views/orderlist.blade.php
+++ resources/views/orderlist.blade.php
@@ -3,26 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <title>Picų sąrašas</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 </head>
 <body>
 
-<table style="border-color: #5e5e5e; background-color: #dfdfdf ; border-style: dashed;">
-    @foreach($list as $item)
-        <tr>
-            <td> {{$item['name']}}</td>
-            <td> {{$item['phone']}}</td>
-            <td>{{$item['crusts']['name']}}</td>
-            <td>{{$item['cheeses']['name']}}</td>
-            <td>
-                @foreach($item['ingredients'] as $ingredient)
-                    {{ $ingredient['ingredients']['name'] }}<br/>
-                @endforeach </td>
-            <td> {{$item['comment']}}</td>
-            <td><a href="{{ action('PizzasController@edit', $item['id']) }}"><button>Redaguoti</button></a></td>
+<div style="border-style:solid; margin: 20px; border-radius: 10px">
+    <table class="table table-striped">
+        <tr class="table-info">
+            <th>Užsakovo vardas</th>
+            <th>Telefono nr.</th>
+            <th>Picos padas</th>
+            <th>Sūris</th>
+            <th>Ingredientai</th>
+            <th>Komentaras</th>
+            <th>Veiksmas</th>
         </tr>
-    @endforeach
-</table>
+        @foreach($list as $item)
+            <tr>
+                <td> {{$item['name']}}</td>
+                <td> {{$item['phone']}}</td>
+                <td>{{$item['crusts']['name']}}</td>
+                <td>{{$item['cheeses']['name']}}</td>
+                <td>
+                    @foreach($item['ingredients'] as $ingredient)
+                        {{ $ingredient['ingredients']['name'] }}<br/>
+                    @endforeach </td>
+                <td> {{$item['comment']}}</td>
+                <td><a href="{{ action('PizzasController@edit', $item['id']) }}">
+                        <button class="table-warning" style="border-radius: 10px">Redaguoti</button>
+                    </a></td>
+            </tr>
+        @endforeach
+    </table>
+</div>
 
-<br/>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
 </body>
 </html>
